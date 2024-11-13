ab0b81d8581ac9a3a59545b2fab2b5400d93970f
Jeong Seon U <sw.jeong97@gmail.com>
2024-11-13 11:51:12 +0900

    Init project

---
+++ src/main/java/org/example/schedule_manage_be_develop/ScheduleManageBeDevelopApplication.java
@@ -0,0 +1,13 @@
+package org.example.schedule_manage_be_develop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleManageBeDevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleManageBeDevelopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/schedule_manage_be_develop/ScheduleManageBeDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.schedule_manage_be_develop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleManageBeDevelopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
