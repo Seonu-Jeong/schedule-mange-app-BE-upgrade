d90cbc43bc4d5d05d6c5e72f85db79547fc6f3da
Jeong Seon U <sw.jeong97@gmail.com>
2024-11-13 18:24:56 +0900

    Feat : Add TimeBaseEntity for JPA Auditing

---
+++ src/main/java/org/example/schedule/entiry/TimeBaseEntity.java
@@ -0,0 +1,26 @@
+package org.example.schedule.entiry;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class TimeBaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false, name = "created_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(updatable = false, name = "modified_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime modifiedAt;
+}
